36ad7dcd1b1342773f0ea6563cbcdbd550c5e338
jeevan15498 <jeevan15498@gmail.com>
2018-05-31 16:30:21 +0530

    Insert Data (User Registration with Database)

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -16,10 +16,27 @@ class UsersController extends AppController
      */
     public function registration()
     {
+        # Set Page Title
         $this->set('title', 'User Registration');
 
-        # Using A Different Validation Set
         $users = $this->Users->newEntity($this->request->getData(), ['validate' => 'register']);
+        if ($this->request->is('post'))
+        {
+            if (empty($users->getErrors()))
+            {
+                $users = $this->Users->patchEntity($users, $this->request->getData(), ['validate' => 'register']);
+                
+                # Set Data in Table
+                $users->is_active = 1;
+    
+                # Save Data
+                if ($this->Users->save($users)) {
+                    $this->Flash->success(__('User has been saved.'));
+                    return $this->redirect(['action' => 'registration']);
+                }
+                $this->Flash->error(__('Unable to add your user.'));
+            }
+        }
         // Set this errors in fields
         $this->set('users', $users);
     }
